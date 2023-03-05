466d34a7087ed86ace459c9b85cfb78d4ae7ce82
ricardoparralive <ricardoparralive@gmail.com>
2023-03-05 10:28:29 -0400

    suma en py

---
+++ tarea_suma.py
@@ -0,0 +1,8 @@
+num1 = float(input("Ingresa el primer número: "))
+num2 = float(input("Ingresa el segundo número: "))
+
+# Suma los dos números
+resultado = num1 + num2
+
+# Imprime el resultado
+print("La suma de", num1, "y", num2, "es:", resultado)
